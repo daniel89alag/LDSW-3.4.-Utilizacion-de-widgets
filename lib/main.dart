76f5d18b819b26cff3df9e370d192a945ec4ef39
daniel <daniel.alvarado@udgvirtual.udg.mx>
2023-11-16 23:57:58 -0600

    integracion de bases de datos

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,14 @@ import 'package:app_udg/models/Gif.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(PantallaEjemplos());
 }
 
